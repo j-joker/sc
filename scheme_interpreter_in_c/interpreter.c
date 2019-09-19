3808ac8c7e81c1a2334ec0d0277370d8e25885eb
j-joker <zlxjune@gmail.com>
2019-09-20 02:17:04 +0800

    add the macro functionn

--- scheme_interpreter_in_c/interpreter.c
+++ scheme_interpreter_in_c/interpreter.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
-#include<string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 enum
 {
 	T_INT,
@@ -19,7 +19,7 @@ enum
 
 };
 struct Obj;
-typedef struct Obj *Primitive(struct Obj *env ,struct Obj *args);
+typedef struct Obj *Primitive(struct Obj *env, struct Obj *args);
 
 typedef struct Obj
 {
@@ -39,7 +39,8 @@ typedef struct Obj
 			struct Obj *vars;
 			struct Obj *up;
 		};
-		struct {
+		struct
+		{
 			struct Obj *params;
 			struct Obj *body;
 			struct Obj *env;
@@ -51,13 +52,15 @@ Obj *read(FILE *in);
 Obj *intern(char *);
 Obj *make_prim(Primitive *);
 Obj *make_int(int);
-Obj *eval_list(Obj *,Obj *);
-Obj *progn(Obj *,Obj *);
-Obj *eval(Obj *,Obj *);
-Obj *find(Obj *,Obj *);
-Obj *make_env(Obj *,Obj *);
-Obj *make_function(Obj *,Obj *,Obj *);
-void add_var(Obj *,Obj *,Obj *);
+Obj *eval_list(Obj *, Obj *);
+Obj *progn(Obj *, Obj *);
+Obj *eval(Obj *, Obj *);
+Obj *find(Obj *, Obj *);
+Obj *make_env(Obj *, Obj *);
+Obj *push_env(Obj *, Obj *, Obj *);
+Obj *make_function(Obj *, Obj *, Obj *, int);
+void add_var(Obj *, Obj *, Obj *);
+void print(Obj *);
 
 static Obj *Nil;
 static Obj *Dot;
@@ -70,132 +73,181 @@ static Obj *env;
 //符号表
 static Obj *symbols;
 
-
-static void error(char *err){
-	fprintf(stderr,err);
+static void error(char *err)
+{
+	fprintf(stderr, err);
 	exit(1);
 }
-static void show_env(Obj *env){
-	Obj *vars=env->vars;
-	
+static void show_env(Obj *env)
+{
+	Obj *vars = env->vars;
 }
 
-int list_length(Obj *list){
-	int len=0;
-	while(1){
-	if(list==Nil){
-		return len;
+int list_length(Obj *list)
+{
+	int len = 0;
+	while (1)
+	{
+		if (list == Nil)
+		{
+			return len;
+		}
+		list = list->cdr;
+		len++;
 	}
-	list=list->cdr;
-	len++;
-	 }
 }
 
-Obj *prim_quote(Obj *env,Obj *args){
+Obj *prim_quote(Obj *env, Obj *args)
+{
 	return args->car;
 }
 
-Obj *prim_plus(Obj *env,Obj *args){
-	int sum=0;
-	args=eval_list(env,args);
-	while(args!=Nil){
-		sum+=args->car->value;
-		args=args->cdr;
-	}	
+Obj *prim_plus(Obj *env, Obj *args)
+{
+	int sum = 0;
+	args = eval_list(env, args);
+	while (args != Nil)
+	{
+		sum += args->car->value;
+		args = args->cdr;
+	}
 	return make_int(sum);
 }
 
-Obj *prim_list(Obj *env,Obj *args){
-	return eval_list(env,args);
+Obj *prim_list(Obj *env, Obj *args)
+{
+	return eval_list(env, args);
 }
 
-Obj *prim_define(Obj *env,Obj *args){
+Obj *prim_define(Obj *env, Obj *args)
+{
 
-	Obj *sym=args->car;
-	Obj *val=args->cdr->car;
-	Obj *new_val=eval(env,val);
-	add_var(env,sym,new_val);
+	Obj *sym = args->car;
+	Obj *val = args->cdr->car;
+	Obj *new_val = eval(env, val);
+	add_var(env, sym, new_val);
 	return new_val;
 }
 
-Obj *prim_lambda(Obj *env,Obj *args){
-	Obj *params=args->car;
-	Obj *body=args->cdr;
-	return make_function(params,body,env);
+Obj *prim_defmacro(Obj *env, Obj *args)
+{
+	Obj *sym = args->car;
+	Obj *params = args->cdr->car;
+	Obj *body = args->cdr->cdr->car;
+	Obj *fn = make_function(params, body, env, T_MACRO);
+	add_var(env, sym, fn);
+	return fn;
 }
 
-Obj *prim_setq(Obj *env,Obj *args){
-	Obj *sym=args->car;
-	Obj *val=args->cdr->car;
-	Obj *o=find(env,sym);
-	if(o==Nil){
-		printf("unknown symbol: %s",sym->str);
+Obj *prim_macro_expand(Obj *env, Obj *args)
+{
+	Obj *fn = eval(env, args->car->car);
+	Obj *params = fn->params;
+	Obj *body = fn->body;
+	Obj *new_env = push_env(fn->env, params, args->car->cdr);
+	Obj *new_exp = eval(new_env, body);
+	return new_exp;
+}
+
+Obj *prim_if(Obj *env, Obj *args)
+{
+	Obj *if_part = eval(env, args->car);
+	Obj *then_part = eval(env, args->cdr->car);
+	Obj *else_part = eval(env, args->cdr->cdr->car);
+	if (if_part->value != 0)
+		return then_part;
+	else
+		return else_part;
+}
+
+Obj *prim_lambda(Obj *env, Obj *args)
+{
+	Obj *params = args->car;
+	Obj *body = args->cdr;
+	return make_function(params, body, env, T_FUNCTION);
+}
+
+Obj *prim_setq(Obj *env, Obj *args)
+{
+	Obj *sym = args->car;
+	Obj *val = args->cdr->car;
+	Obj *o = find(env, sym);
+	if (o == Nil)
+	{
+		printf("unknown symbol: %s", sym->str);
 		exit(1);
 	}
-	Obj *new_arg=eval(env,val);
-	o->cdr=new_arg;
+	Obj *new_arg = eval(env, val);
+	o->cdr = new_arg;
 	return new_arg;
 }
 
-
 Obj *make_sym(char *);
-Obj *cons(Obj *car,Obj *cdr){
-	Obj *o =malloc(sizeof(Obj));
-	o->car=car;
-	o->cdr=cdr;
-	o->type=T_CELL;
+Obj *cons(Obj *car, Obj *cdr)
+{
+	Obj *o = malloc(sizeof(Obj));
+	o->car = car;
+	o->cdr = cdr;
+	o->type = T_CELL;
 	return o;
 }
 
-void add_var(Obj *env,Obj *sym ,Obj *val){
-	env->vars=cons(cons(sym,val),env->vars);
+void add_var(Obj *env, Obj *sym, Obj *val)
+{
+	env->vars = cons(cons(sym, val), env->vars);
 }
-void add_prim(Obj *env,char *name ,Primitive *fn){
-	Obj *sym=intern(name);
+void add_prim(Obj *env, char *name, Primitive *fn)
+{
+	Obj *sym = intern(name);
 	Obj *fun = make_prim(fn);
-	add_var(env,sym,fun);
+	add_var(env, sym, fun);
 }
-Obj *find(Obj *env,Obj* sym){
-	for(Obj *p=env ;p;p=p->up){
-		for(Obj *x=p->vars;x!=Nil;x=x->cdr){
-			Obj *bind=x->car;
-			if(bind->car==sym){
+Obj *find(Obj *env, Obj *sym)
+{
+	for (Obj *p = env; p; p = p->up)
+	{
+		for (Obj *x = p->vars; x != Nil; x = x->cdr)
+		{
+			Obj *bind = x->car;
+			if (bind->car == sym)
+			{
 				return bind;
 			}
 		}
 	}
 	return Nil;
-	
 }
 
 Obj *intern(char *name)
 {
 	for (Obj *p = symbols; p != Nil; p = p->cdr)
 	{
-			if(strcmp(p->car->str,name)==0){
-				return p->car;
-			}
+		if (strcmp(p->car->str, name) == 0)
+		{
+			return p->car;
+		}
 	}
-	Obj *sym=make_sym(name);
-	symbols=cons(sym,symbols);
+	Obj *sym = make_sym(name);
+	symbols = cons(sym, symbols);
 	return sym;
 }
 
 Obj *make_sym(char *name)
 {
 	Obj *o = malloc(sizeof(Obj));
-	o->str =malloc(strlen(name)+1);
-	strcpy(o->str,name);
+	o->str = malloc(strlen(name) + 1);
+	strcpy(o->str, name);
 	o->type = T_SYMBOL;
 	return o;
 }
 
-Obj *make_function(Obj *params,Obj *body,Obj *env){
-	Obj *o=malloc(sizeof(Obj));
-	o->params=params;
-	o->body=body;
-	o->env=env;
-	o->type=T_FUNCTION;
+Obj *make_function(Obj *params, Obj *body, Obj *env, int type)
+{
+	Obj *o = malloc(sizeof(Obj));
+	o->params = params;
+	o->body = body;
+	o->env = env;
+	o->type = type;
 	return o;
 }
 
@@ -206,17 +258,18 @@ Obj *make_special(int type)
 	return o;
 }
 
-Obj *push_env(Obj *env,Obj *vars,Obj *vals){
-	Obj *p=vars,*q=vals;
-	Obj *frame=Nil;
-	while(p!=Nil&&q!=Nil){
-		Obj *bind=cons(p->car,q->car);
-		frame=cons(bind,frame);
-		p=p->cdr;
-		q=q->cdr;
+Obj *push_env(Obj *env, Obj *vars, Obj *vals)
+{
+	Obj *p = vars, *q = vals;
+	Obj *frame = Nil;
+	while (p != Nil && q != Nil)
+	{
+		Obj *bind = cons(p->car, q->car);
+		frame = cons(bind, frame);
+		p = p->cdr;
+		q = q->cdr;
 	}
-	return make_env(frame,env);
-	
+	return make_env(frame, env);
 }
 
 Obj *make_env(Obj *vars, Obj *up)
@@ -227,51 +280,71 @@ Obj *make_env(Obj *vars, Obj *up)
 	return env;
 }
 
-Obj *make_int(int num){
-	Obj *o=malloc(sizeof(Obj));
-	o->type=T_INT;
-	o->value=num;
+Obj *make_int(int num)
+{
+	Obj *o = malloc(sizeof(Obj));
+	o->type = T_INT;
+	o->value = num;
 	return o;
 }
-Obj *make_prim(Primitive *fn){
-	Obj *o =malloc(sizeof(Obj));
-	o->type=T_PRIMITIVE;
-	o->fn=fn;
+Obj *make_prim(Primitive *fn)
+{
+	Obj *o = malloc(sizeof(Obj));
+	o->type = T_PRIMITIVE;
+	o->fn = fn;
 	return o;
 }
 
-Obj *apply(Obj *env,Obj *fn ,Obj  *args){
-	if(fn->type==T_PRIMITIVE){
-		return fn->fn(env,args);
-	}else if(fn->type==T_FUNCTION){
-		Obj *params=fn->params;
-		Obj *body=fn->body;
-		Obj *env=fn->env;
-		Obj *new_args=eval_list(env,args);
-		Obj *new_env=push_env(env,params,new_args);
-		return progn(new_env,body);
+Obj *apply(Obj *env, Obj *fn, Obj *args)
+{
+	if (fn->type == T_PRIMITIVE)
+	{
+		return fn->fn(env, args);
+	}
+	else if (fn->type == T_FUNCTION)
+	{
+		Obj *params = fn->params;
+		Obj *body = fn->body;
+		Obj *env = fn->env;
+		Obj *new_args = eval_list(env, args);
+		Obj *new_env = push_env(env, params, new_args);
+		Obj *re = progn(new_env, body);
+		return re;
+	}
+	else
+	{
+		Obj *params = fn->params;
+		Obj *body = fn->body;
+		Obj *new_env = push_env(fn->env, params, args);
+		Obj *new_exp = eval(new_env, body);
+		return eval(env, new_exp);
 	}
 }
-Obj *progn(Obj *env,Obj *list){
-	Obj *re=Nil;
-	for(Obj *p=list;p!=Nil;p=p->cdr){
-		re=eval(env,p->car);
+Obj *progn(Obj *env, Obj *list)
+{
+	Obj *re = Nil;
+	for (Obj *p = list; p != Nil; p = p->cdr)
+	{
+		re = eval(env, p->car);
 	}
 	return re;
 }
 
-
-Obj *eval_list(Obj *env,Obj *list){
-	Obj *head=NULL,*tail=NULL;
-	for(Obj *p=list;p!=Nil;p=p->cdr){
-		Obj * o=eval(env,p->car);
-		if(head==NULL){
-			head=tail=cons(o,Nil);
-		}else{
-			tail->cdr=cons(o,Nil);
-			tail=tail->cdr;
+Obj *eval_list(Obj *env, Obj *list)
+{
+	Obj *head = NULL, *tail = NULL;
+	for (Obj *p = list; p != Nil; p = p->cdr)
+	{
+		Obj *o = eval(env, p->car);
+		if (head == NULL)
+		{
+			head = tail = cons(o, Nil);
 		}
-		
+		else
+		{
+			tail->cdr = cons(o, Nil);
+			tail = tail->cdr;
+		}
 	}
 	return head;
 }
@@ -285,21 +358,23 @@ Obj *eval(Obj *env, Obj *obj)
 	case T_NIL:
 	case T_PAREN:
 		return obj;
-	case T_SYMBOL:{
-		Obj *bind=find(env,obj);
-		if(bind==Nil){
-			printf("symbol %s is not defined",obj->str);
+	case T_SYMBOL:
+	{
+		Obj *bind = find(env, obj);
+		if (bind == Nil)
+		{
+			printf("symbol %s is not defined", obj->str);
 			exit(1);
 		}
 		return bind->cdr;
 		return obj;
 	}
 
-		
-	case T_CELL:{
-		Obj *fn =eval(env,obj->car);
-		Obj *args=obj->cdr;
-		return apply(env,fn,args);
+	case T_CELL:
+	{
+		Obj *fn = eval(env, obj->car);
+		Obj *args = obj->cdr;
+		return apply(env, fn, args);
 	}
 	default:
 		return NULL;
@@ -311,112 +386,138 @@ void print(Obj *o)
 
 	switch (o->type)
 	{
+	case T_NIL:
+		printf("()");
+		break;
 	case T_FUNCTION:
 		printf("<function>");
+		return;
+	case T_MACRO:
+		printf("<macro>");
 		return;
 	case T_INT:
 		printf("%d", o->value);
 		return;
 	case T_SYMBOL:
-		printf("%s",o->str);
+		printf("%s", o->str);
 		break;
 	case T_TRUE:
 		printf("#t");
 		break;
 	case T_CELL:
 		printf("(");
-		while(1){
-			if(o->type==T_CELL){
+		while (1)
+		{
+			if (o->type == T_CELL)
+			{
 				print(o->car);
-			}else{
+			}
+			else
+			{
 				print(o);
 			}
-			if(o->type!=T_CELL||o->cdr==Nil){
-				  break;
+			if (o->type != T_CELL || o->cdr == Nil)
+			{
+				break;
 			}
-			if(o->cdr->type!=T_CELL){
+			if (o->cdr->type != T_CELL)
+			{
 				printf(" . ");
-			}else {
+			}
+			else
+			{
 				printf(" ");
 			}
-			o=o->cdr;
-			
+			o = o->cdr;
 		}
 		printf(")");
 		break;
+	default:
+	printf("unkonw type\n");
+	break;
 	}
-	
 }
-int peek(FILE *in){
-	int c=getc(in);
-	ungetc(c,in);
+int peek(FILE *in)
+{
+	int c = getc(in);
+	ungetc(c, in);
 	return c;
 }
 
-Obj *read_quote(FILE *in){
-	Obj *sym=intern("quote");
-	Obj *o =read(in);
-	return cons(sym,cons(o,Nil));
-
+Obj *read_quote(FILE *in)
+{
+	Obj *sym = intern("quote");
+	Obj *o = read(in);
+	return cons(sym, cons(o, Nil));
 }
 
-Obj * read_sym(FILE *in){
+Obj *read_sym(FILE *in)
+{
 	char buf[1024];
-	int c=getc(in);
-	int i=0;
-	while(isalpha(c)||c=='+'){
-		buf[i++]=c;
-		c=getc(in);
+	int c = getc(in);
+	int i = 0;
+	while (isalpha(c) || c == '+')
+	{
+		buf[i++] = c;
+		c = getc(in);
 	}
-	ungetc(c,in);
-	buf[i]='\0';
-	Obj* o=intern(buf);
+	ungetc(c, in);
+	buf[i] = '\0';
+	Obj *o = intern(buf);
 	return o;
 }
 
-int read_num(FILE *in){
+int read_num(FILE *in)
+{
 	int c;
-	int sum=0;
-	int sign=1;
-	c=getc(in);
-	while(isdigit(c)||c=='-'){
-		if(c=='-'){
-			sign=-1;
-			c=getc(in);
+	int sum = 0;
+	int sign = 1;
+	c = getc(in);
+	while (isdigit(c) || c == '-')
+	{
+		if (c == '-')
+		{
+			sign = -1;
+			c = getc(in);
 			continue;
 		}
-		sum=sum*10+c-'0';
-		c=getc(in);
+		sum = sum * 10 + c - '0';
+		c = getc(in);
 	}
-	sum*=sign;
-	ungetc(c,in);
+	sum *= sign;
+	ungetc(c, in);
 	return sum;
 }
 
-Obj *read_list(FILE *in){
-	int c=getc(in);
-	if(c==')')
+Obj *read_list(FILE *in)
+{
+	int c = getc(in);
+	if (c == ')')
 		return Nil;
-	ungetc(c,in);
+	ungetc(c, in);
 
-	Obj *head,*tail;
-	Obj *o=read(in);
-	head=tail=cons(o,Nil);
-	while(1){
-		o=read(in);
-		if(o==CParen){
+	Obj *head, *tail;
+	Obj *o = read(in);
+	head = tail = cons(o, Nil);
+	while (1)
+	{
+		o = read(in);
+		if (o == CParen)
+		{
 			return head;
 		}
-		if(o==Dot){
-			o=read(in);
-			tail->cdr=o;
-			if(read(in)!=CParen){
+		if (o == Dot)
+		{
+			o = read(in);
+			tail->cdr = o;
+			if (read(in) != CParen)
+			{
 				printf("parenthfesis not closed\n");
 			}
 			return head;
 		}
-		tail->cdr=cons(o,Nil);
-		tail=tail->cdr;
+		tail->cdr = cons(o, Nil);
+		tail = tail->cdr;
 	}
 }
 
@@ -428,46 +529,57 @@ Obj *read(FILE *in)
 		c = getc(in);
 		if (isspace(c))
 			continue;
-		if (c ==EOF)
+		if (c == EOF)
 			return NULL;
-		if(c=='\''){
+		if (c == '\'')
+		{
 			return read_quote(in);
 		}
-		if(c=='.'){
+		if (c == '.')
+		{
 			return Dot;
 		}
-		if(c==')'){
+		if (c == ')')
+		{
 			return CParen;
 		}
-		if(isdigit(c)||c=='-'){
-			ungetc(c,in);
-			Obj *num=make_int(read_num(in));
+		if (isdigit(c) || c == '-')
+		{
+			ungetc(c, in);
+			Obj *num = make_int(read_num(in));
 			return num;
 		}
-		if(isalpha(c)||c=='+'){
-			ungetc(c,in);
-			Obj * sym=read_sym(in);
+		if (isalpha(c) || c == '+')
+		{
+			ungetc(c, in);
+			Obj *sym = read_sym(in);
 			return sym;
 		}
-		if(c=='('){
-			Obj *o =read_list(in);
+		if (c == '(')
+		{
+			Obj *o = read_list(in);
 			return o;
 		}
 	}
 }
 
-void define_constant(Obj *env){
-	Obj *sym=intern("t");
-	add_var(env,sym,True);
+void define_constant(Obj *env)
+{
+	Obj *sym = intern("t");
+	add_var(env, sym, True);
 }
 
-void define_prim_function(Obj *env){
-	add_prim(env,"quote",prim_quote);
-	add_prim(env,"+",prim_plus);
-	add_prim(env,"list",prim_list);
-	add_prim(env,"setq",prim_setq);
-	add_prim(env,"define",prim_define);
-	add_prim(env,"lambda",prim_lambda);
+void define_prim_function(Obj *env)
+{
+	add_prim(env, "quote", prim_quote);
+	add_prim(env, "+", prim_plus);
+	add_prim(env, "list", prim_list);
+	add_prim(env, "setq", prim_setq);
+	add_prim(env, "define", prim_define);
+	add_prim(env, "lambda", prim_lambda);
+	add_prim(env, "if", prim_if);
+	add_prim(env, "defmacro", prim_defmacro);
+	add_prim(env, "macroexpand", prim_macro_expand);
 }
 int main()
 {
@@ -476,21 +588,22 @@ int main()
 	Dot = make_special(T_DOT);
 	Par = make_special(T_PAREN);
 	True = make_special(T_TRUE);
-	CParen=make_special(T_PAREN);
+	CParen = make_special(T_PAREN);
 	//符号池
-	symbols=Nil;
+	symbols = Nil;
 	//环境
-	env = make_env(Nil, NULL);		
+	env = make_env(Nil, NULL);
 	//内置的value
 	define_constant(env);
 	//内置的函数
 	define_prim_function(env);
 
 	printf("******Welcom to Lisp World******\n\n");
-	while(1){
+	while (1)
+	{
 		printf(">");
-		Obj *exp=read(stdin);
-		print(eval(env,exp));
+		Obj *exp = read(stdin);
+		print(eval(env, exp));
 		printf("\n");
 	}
 
